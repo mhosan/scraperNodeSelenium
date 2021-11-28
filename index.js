c2486f6200da85dc97fad1ef3af181224bc4f442
marcelo <mhosan@gmail.com>
2021-11-28 19:15:20 -0300

    nada

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-const {Builder} = require('selenium-webdriver');
-const firefox = require('selenium-webdriver/firefox');
+const {Builder, By} = require('selenium-webdriver');
+/* const firefox = require('selenium-webdriver/firefox');
 const proxy = require('selenium-webdriver/proxy');
-
 const options = new firefox.Options();
 options.setPreference('browser.download.dir', 'C:\\seleniumDownloads');
 options.setPreference('browser.download.folderList', 2);
-options.setPreference('browser.helperApps.neverAsk.saveToDisk', 'application/x-csv'); /*ojo con los content-type. Ver en el navegador
-el tipo de archivo que se quiere bajar y agregar aqui con una coma:  */ 
+options.setPreference('browser.helperApps.neverAsk.saveToDisk', 'application/x-csv');
+/*ojo con los content-type. Ver en el navegador
+, con f12, en network, el tipo de archivo que se quiere bajar y agregar aqui con una coma:*/ 
 
-const proxyServer = '185.104.252.10:9090';
-
+/* const proxyServer = '185.104.252.10:9090';
 const driver = new Builder()
     .forBrowser('firefox')
     .setFirefoxOptions(options)
@@ -18,5 +17,29 @@ const driver = new Builder()
         https: proxyServer
     }))
     .build();
-
-driver.get('http://google.com/');
+ */
+const driver = new Builder()
+    .forBrowser('firefox')
+    .build();
+//driver.get('http://google.com/');
+/* async function openWebSites(){
+    try{
+        await driver.get('http://yahoo.com/');
+        await driver.get('http://google.com/');
+    }catch(error){
+        console.log(error);
+    }
+}
+openWebSites();
+ */
+async function simpleRegistration(){
+    try{
+        await driver.get('https://rori4.github.io/selenium-practice/#/pages/practice/simple-registration');
+        await driver.findElement(By.name("email")).sendKeys("example@gmail.com");
+        await driver.findElement(By.id("password")).sendKeys("12345");
+        await driver.findElement(By.name("submit")).click();
+    }catch(error){
+        console.log(error);
+    } 
+}
+simpleRegistration();
